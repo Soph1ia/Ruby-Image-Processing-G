bed17505bd51806a47fc553ab2b9d9a8409751fe
S0ph1ia <badalovasophia@gmail.com>
2021-03-29 12:57:39 +0100

    Added resize code that is integrated properly

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "functions_framework"
 require "json"
+require "chunky_png"
 
 # This function receives an HTTP request of type Rack::Request
 # and interprets the body as JSON. It prints the contents of
@@ -7,14 +8,16 @@ require "json"
 FunctionsFramework.http "hello_world" do |request|
   input = JSON.parse request.body.read rescue {}
   msg = input["message"].to_s
-
+  image_resizer = Rotate_Image.new
+  image_resizer.rotate
+  output = "Resized image successfully"
   return output.to_s
 end
 
 class Rotate_Image
 
   def rotate
-    image = ChunkyPNG::Image.from_file('image.jpg')
+    image = ChunkyPNG::Image.from_file('image.png')
     resized_image = image.resize(1024,1000)
     return resized_image
   end
